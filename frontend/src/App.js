309c8591300b8bbcc5b2d8ab608067c4d1ad1520
simple <michel.diab162@gmail.com>
2024-08-24 19:13:56 +0300

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,13 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-
-
 function App() {
     const [barcodeData, setBarcodeData] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`http://localhost:5000/api/barcodes`);
+                const response = await axios.get('http://localhost:5000/api/barcodes');
                 setBarcodeData(Object.entries(response.data));
             } catch (error) {
                 console.error('Error fetching barcode data:', error);
@@ -34,9 +32,9 @@ function App() {
             <main>
                 {barcodeData.map(([barcode, { imageURL, qrCode }]) => (
                     <div key={barcode} className="image-container">
-                         <img src={`https://localhost:5000${imageURL}`} alt={`Barcode ${barcode}`} />
+                        <img src={`http://localhost:5000${imageURL}`} alt={`Barcode ${barcode}`} />
                         <img src={qrCode} alt={`QR Code ${barcode}`} />
-                        <a href={`https://localhost:5000${imageURL}`} download className="download-button">
+                        <a href={`http://localhost:5000${imageURL}`} download className="download-button">
                             Download Image
                         </a>
                     </div>
